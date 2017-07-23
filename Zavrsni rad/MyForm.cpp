29f9d0579f2c3ff87aaca94efbe55d681a591382
Dujekrst <Dujekrst@DUJE>
2017-07-23 16:26:27 +0200

    Add project files.

---
+++ Zavrsni rad/MyForm.cpp
@@ -0,0 +1,22 @@
+#include "MyForm.h"
+#include <stdlib.h>   
+#include <ctype.h>
+#include <Exception>
+#include <complex>
+#include <Windows.h>
+#include <stdio.h>
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Zavrsnirad::MyForm form;
+	Application::Run(%form);
+	
+
+
+}
---
+++ Zavrsni rad/MyForm.h
@@ -0,0 +1,417 @@
+﻿#pragma once
+
+namespace Zavrsnirad {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::TextBox^  textBox6;
+	private: System::Windows::Forms::TextBox^  text_box_rezultat;
+	private: array<int> ^niz;
+	
+	
+	private: System::Windows::Forms::Label^  label7;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	protected: 
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->text_box_rezultat = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(95, 242);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Izračunaj";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->AccessibleDescription = L"";
+			this->textBox1->AccessibleName = L"";
+			this->textBox1->ForeColor = System::Drawing::Color::DimGray;
+			this->textBox1->Location = System::Drawing::Point(84, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->Text = L"Unesite parametar";
+			this->textBox1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox1_MouseClick);
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(7, 12);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(71, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Visina antene";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(7, 48);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(75, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Dubina snjega";
+			// 
+			// textBox2
+			// 
+			this->textBox2->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->textBox2->Location = System::Drawing::Point(84, 48);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 3;
+			this->textBox2->Text = L"Unesite parametar";
+			this->textBox2->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox2_MouseClick);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(7, 84);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(71, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Dielektricnost";
+			// 
+			// textBox3
+			// 
+			this->textBox3->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->textBox3->Location = System::Drawing::Point(84, 84);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 5;
+			this->textBox3->Text = L"Unesite parametar";
+			this->textBox3->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox3_MouseClick);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(7, 123);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(52, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Vodljivost";
+			// 
+			// textBox4
+			// 
+			this->textBox4->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->textBox4->Location = System::Drawing::Point(84, 123);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 7;
+			this->textBox4->Text = L"Unesite parametar";
+			this->textBox4->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox4_MouseClick);
+			this->textBox4->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox4_TextChanged);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(7, 159);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(103, 13);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"Dielektricnost zemlje";
+			// 
+			// textBox5
+			// 
+			this->textBox5->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->textBox5->Location = System::Drawing::Point(84, 175);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 20);
+			this->textBox5->TabIndex = 9;
+			this->textBox5->Text = L"Unesite parametar";
+			this->textBox5->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox5_MouseClick);
+			this->textBox5->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox5_TextChanged);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(7, 196);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(84, 13);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"Vodljivost zemlje";
+			// 
+			// textBox6
+			// 
+			this->textBox6->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->textBox6->Location = System::Drawing::Point(84, 212);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(100, 20);
+			this->textBox6->TabIndex = 11;
+			this->textBox6->Text = L"Unesite parametar";
+			this->textBox6->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::textBox6_MouseClick);
+			// 
+			// text_box_rezultat
+			// 
+			this->text_box_rezultat->Location = System::Drawing::Point(402, 61);
+			this->text_box_rezultat->Name = L"text_box_rezultat";
+			this->text_box_rezultat->Size = System::Drawing::Size(100, 20);
+			this->text_box_rezultat->TabIndex = 13;
+			this->text_box_rezultat->Text = L" ";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(303, 61);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(46, 13);
+			this->label7->TabIndex = 14;
+			this->label7->Text = L"Rezultat";
+			this->label7->Click += gcnew System::EventHandler(this, &MyForm::label7_Click);
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(216, 99);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series1->Legend = L"Legend1";
+			series1->Name = L"Ulazne vrijednosti";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(509, 459);
+			this->chart1->TabIndex = 15;
+			this->chart1->Text = L"chart1";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(95, 285);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 16;
+			this->button2->Text = L"Aktiviraj diagram";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(95, 361);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 17;
+			this->button3->Text = L"Ponovi unos";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(704, 554);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(89, 34);
+			this->button4->TabIndex = 18;
+			this->button4->Text = L"Exit";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(858, 622);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->chart1);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->text_box_rezultat);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"Zavrsni rad";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &MyForm::MyForm_KeyPress);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 String ^ para1 = this->textBox1->Text;
+				 String ^ para2 = this->textBox2->Text;
+				 String ^ para3 = this->textBox3->Text;
+				 String ^ para4 = this->textBox4->Text;
+				 String ^ para5 = this->textBox5->Text;
+				 String ^ para6 = this->textBox6->Text;
+				 int zbroj = 0;
+	niz= gcnew array<int>(9);
+				 try{
+					 niz[0] = Convert::ToInt32(para1);
+					 niz[1] = Convert::ToInt32(para2);
+					 niz[2] = Convert::ToInt32(para3);
+					 niz[3] = Convert::ToInt32(para4);
+					 niz[4] = Convert::ToInt32(para5);
+					 niz[5] = Convert::ToInt32(para6);
+				 }
+				 catch(...)
+				 {
+
+System:MessageBox::Show("Unesite tocne parametre!");
+				 }
+				 for(int i = 0; i<=5;i++)
+				 {
+					 zbroj += niz[i];
+				 }
+				 String ^ rezultat = " ";
+				 rezultat = Convert::ToString(zbroj);
+				 this->text_box_rezultat->Text = rezultat;
+
+			 }
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void textBox4_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void textBox5_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void label7_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void MyForm_KeyPress(System::Object^  sender, System::Windows::Forms::KeyPressEventArgs^  e) {
+			 }
+			 //Hover click
+	private: System::Void textBox1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox1->Text = L"";
+				this->textBox1->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void textBox2_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox2->Text = L"";
+				 this->textBox2->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void textBox3_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox3->Text = L"";
+				 this->textBox3->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void textBox4_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox4->Text = L"";
+				 this->textBox4->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void textBox5_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox5->Text = L"";
+				 this->textBox5->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void textBox6_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 this->textBox6->Text = L"";
+				 this->textBox6->ForeColor = System::Drawing::Color::Black;
+			 }
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+				 
+
+				
+				 this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz1",niz[0]);
+			     this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz2",niz[1]);
+			 	 this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz3",niz[2]);
+			 	 this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz4",niz[3]);
+			 	 this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz5",niz[4]);
+			     this->chart1->Series["Ulazne vrijednosti"]->Points->AddXY("Ulaz6",niz[5]);
+
+				 
+			 }
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+		  this->chart1->Series["Ulazne vrijednosti"]->Points->Clear();
+		 }
+private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
+			 Application::Exit();
+		 }
+};
+
+
+}
